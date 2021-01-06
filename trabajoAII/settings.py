0612b1d84dbeca43d1c8a02cd42a5192f7738f87
Javier Solís García <javsolgar@alum.us.es>
2021-01-06 18:23:42 +0100

    tocado formulario de inicio de sesión

--- trabajoAII/settings.py
+++ trabajoAII/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/static/root/'
 MODULES = [
     'application',
     'news_game',
