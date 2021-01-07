03d1643444b5f5e16ead804c143760471110b1be
Javier Solís García <javsolgar@alum.us.es>
2021-01-07 12:33:34 +0100

    Arregladas las pruebas de selenium

--- application/test_selenium.py
+++ application/test_selenium.py
@@ -19,33 +19,44 @@ class TestApplication(LiveServerTestCase):
         driver = self.driver
         driver.get(self.base_url)
 
-        self.assertEqual(u"Iniciar sesión", driver.find_element_by_link_text(u"Iniciar sesión").text)
+        self.assertEqual("Acceder", driver.find_element_by_link_text("Acceder").text)
 
-        driver.find_element_by_link_text(u"Iniciar sesión").click()
-        driver.find_element_by_id("id_username").click()
+        driver.find_element_by_link_text("Acceder").click()
+        self.assertEqual(u"Inicio de sesión", driver.find_element_by_link_text(u"Inicio de sesión").text)
+
+        driver.find_element_by_link_text(u"Inicio de sesión").click()
+        self.assertEqual(u"Inicio de sesión", driver.find_element_by_xpath("//div[@id='cover-caption']/div/div/div/form/div/h2").text)
+
         driver.find_element_by_id("id_username").clear()
         driver.find_element_by_id("id_username").send_keys("admin")
         driver.find_element_by_id("id_password").click()
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys("admin")
-        driver.find_element_by_xpath("//button[@type='submit']").click()
+        driver.find_element_by_xpath("//button[@type='btn btn-primary']").click()
+        self.assertEqual("admin", driver.find_element_by_link_text("admin").text)
 
-        self.assertEqual("Opciones para admin:", driver.find_element_by_xpath("//li[4]/b").text)
-        self.assertEqual("Opciones de administrador", driver.find_element_by_xpath("//li[3]/b").text)
+        driver.find_element_by_link_text("admin").click()
         self.assertEqual(u"Cerrar sesión", driver.find_element_by_link_text(u"Cerrar sesión").text)
 
         driver.find_element_by_link_text(u"Cerrar sesión").click()
-        self.assertEqual("Inicio", driver.find_element_by_xpath("//h1").text)
-        self.assertEqual(u"Iniciar sesión", driver.find_element_by_link_text(u"Iniciar sesión").text)
+        self.assertEqual("Acceder", driver.find_element_by_link_text("Acceder").text)
+
+        driver.find_element_by_link_text("Acceder").click()
+        self.assertEqual(u"Inicio de sesión", driver.find_element_by_link_text(u"Inicio de sesión").text)
 
     def test_usuario_ya_registrado(self):
         driver = self.driver
-
         driver.get(self.base_url)
-        self.assertEqual("Inicio", driver.find_element_by_xpath("//h1").text)
+
+        self.assertEqual("Acceder", driver.find_element_by_link_text("Acceder").text)
+
+        driver.find_element_by_link_text("Acceder").click()
         self.assertEqual("Registrarse", driver.find_element_by_link_text("Registrarse").text)
 
         driver.find_element_by_link_text("Registrarse").click()
+        self.assertEqual("Registro de usuario",
+                         driver.find_element_by_xpath("//div[@id='cover-caption']/div/div/div/form/h4").text)
+
         driver.find_element_by_id("id_username").click()
         driver.find_element_by_id("id_username").clear()
         driver.find_element_by_id("id_username").send_keys("prueba")
@@ -56,16 +67,22 @@ class TestApplication(LiveServerTestCase):
         driver.find_element_by_id("id_password2").clear()
         driver.find_element_by_id("id_password2").send_keys("963852741A")
         driver.find_element_by_xpath("//button[@type='submit']").click()
-        self.assertEqual("Registro de usuario", driver.find_element_by_xpath("//h1").text)
-        self.assertEqual("A user with that username already exists.", driver.find_element_by_xpath("//p[2]").text)
+        self.assertEqual("A user with that username already exists.",
+                         driver.find_element_by_xpath("//div[@id='cover-caption']/div/div/div/form/div/p[2]").text)
 
-    def test_registro_usuario(self):
+    def test_registro_inicio_sesion_nuevo_usuario(self):
         driver = self.driver
         driver.get(self.base_url)
-        self.assertEqual("Inicio", driver.find_element_by_xpath("//h1").text)
+
+        # Registro del nuevo usuario
+        self.assertEqual("Acceder", driver.find_element_by_link_text("Acceder").text)
+
+        driver.find_element_by_link_text("Acceder").click()
         self.assertEqual("Registrarse", driver.find_element_by_link_text("Registrarse").text)
 
         driver.find_element_by_link_text("Registrarse").click()
+        self.assertEqual("Registro de usuario",
+                         driver.find_element_by_xpath("//div[@id='cover-caption']/div/div/div/form/h4").text)
         driver.find_element_by_id("id_username").click()
         driver.find_element_by_id("id_username").clear()
         driver.find_element_by_id("id_username").send_keys("test_registro")
@@ -77,14 +94,36 @@ class TestApplication(LiveServerTestCase):
         driver.find_element_by_id("id_password2").clear()
         driver.find_element_by_id("id_password2").send_keys("987456321A")
         driver.find_element_by_xpath("//button[@type='submit']").click()
-        self.assertEqual("Inicio", driver.find_element_by_xpath("//h1").text)
-        self.assertEqual("Opciones para test_registro:", driver.find_element_by_xpath("//li[3]/b").text)
+
+        self.assertEqual("test_registro", driver.find_element_by_link_text("test_registro").text)
+
+        driver.find_element_by_link_text("test_registro").click()
         self.assertEqual(u"Cerrar sesión", driver.find_element_by_link_text(u"Cerrar sesión").text)
 
-        driver.get(self.base_url+'/registro/')
-        self.assertEqual("Inicio", driver.find_element_by_xpath("//h1").text)
-        self.assertEqual("Opciones para test_registro:", driver.find_element_by_xpath("//li[3]/b").text)
+        # Inicio sesión nuevo usuario
+        driver.find_element_by_link_text(u"Cerrar sesión").click()
+        self.assertEqual("Acceder", driver.find_element_by_link_text("Acceder").text)
+
+        driver.find_element_by_link_text("Acceder").click()
+        self.assertEqual(u"Inicio de sesión", driver.find_element_by_link_text(u"Inicio de sesión").text)
+
+        driver.find_element_by_link_text(u"Inicio de sesión").click()
+        self.assertEqual(u"Inicio de sesión",
+                         driver.find_element_by_xpath("//div[@id='cover-caption']/div/div/div/form/div/h2").text)
+
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("test_registro")
+        driver.find_element_by_id("id_password").click()
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("987456321A")
+        driver.find_element_by_xpath("//button[@type='btn btn-primary']").click()
+        self.assertEqual("test_registro", driver.find_element_by_link_text("test_registro").text)
+
+        driver.find_element_by_link_text("test_registro").click()
         self.assertEqual(u"Cerrar sesión", driver.find_element_by_link_text(u"Cerrar sesión").text)
 
         driver.find_element_by_link_text(u"Cerrar sesión").click()
-        self.assertEqual("Registrarse", driver.find_element_by_link_text("Registrarse").text)
+        self.assertEqual("Acceder", driver.find_element_by_link_text("Acceder").text)
+
+        driver.find_element_by_link_text("Acceder").click()
+        self.assertEqual(u"Inicio de sesión", driver.find_element_by_link_text(u"Inicio de sesión").text)
